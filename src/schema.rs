24eba63691735b3c9989bbee0a96f56d9214327d
Dhairya Nadapara <dhairyanadapara98@gmail.com>
2023-08-22 23:15:36 +0530

    auto change

--- src/schema.rs
+++ src/schema.rs
@@ -7,18 +7,3 @@ diesel::table! {
         geojson_data -> Jsonb,
     }
 }
-
-diesel::table! {
-    spatial_ref_sys (srid) {
-        srid -> Int4,
-        #[max_length = 256]
-        auth_name -> Nullable<Varchar>,
-        auth_srid -> Nullable<Int4>,
-        #[max_length = 2048]
-        srtext -> Nullable<Varchar>,
-        #[max_length = 2048]
-        proj4text -> Nullable<Varchar>,
-    }
-}
-
-diesel::allow_tables_to_appear_in_same_query!(geojsons, spatial_ref_sys,);
